a5e3ed9b91fff0fd34b469dd7ab30c30bdb70e72
AmaliePauli <amalie.nyhagen@hotmail.com>
2019-07-05 17:30:22 +0200

    Updated to use flair==0.4.2, and edited decription for Pos_tagger_example to include installation for polyglot

--- danlp/models/pos_taggers.py
+++ danlp/models/pos_taggers.py
@@ -13,6 +13,6 @@ def load_pos_tagger_with_flair(cache_dir=DEFAULT_CACHE_DIR, verbose=False):
     model_weight_path = download_model('flair.pos', cache_dir, verbose=verbose)
 
     # using the flair model
-    flair_model = SequenceTagger.load_from_file(model_weight_path)
+    flair_model = SequenceTagger.load(model_weight_path)
 
     return flair_model
