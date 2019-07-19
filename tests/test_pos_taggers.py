964fa82b6d14d4815486af9abfaa1561df6b6500
Rasmus Hvingelby <rasmus.hvingelby@alexandra.dk>
2019-07-19 13:18:50 +0200

    Add test for Flair POS Tagger

---
+++ tests/test_pos_taggers.py
@@ -0,0 +1,25 @@
+import unittest
+
+from flair.data import Sentence
+
+from danlp.models.pos_taggers import load_pos_tagger_with_flair
+
+
+class TestPosTaggers(unittest.TestCase):
+    def test_flair_tagger(self):
+
+        # Load the POS tagger using the DaNLP wrapper
+        flair_model = load_pos_tagger_with_flair()
+
+        # Using the flair POS tagger
+        sentence = Sentence('jeg hopper på en bil som er rød sammen med Jens-Peter E. Hansen')
+        flair_model.predict(sentence)
+
+        expected_string = "jeg <PRON> hopper <VERB> på <ADP> en <DET> bil <NOUN> som <ADP> er " \
+                          "<AUX> rød <ADJ> sammen <ADV> med <ADP> Jens-Peter <PROPN> E. <PROPN> Hansen <PROPN>"
+
+        self.assertEqual(sentence.to_tagged_string(), expected_string)
+
+
+if __name__ == '__main__':
+    unittest.main()
