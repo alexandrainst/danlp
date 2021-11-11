b14022b7b910381b097c2e8aeb351291abf33c79
ophelielacroix <ophelie.lacroix6@gmail.com>
2021-11-11 18:02:53 +0100

    add test

--- tests/test_bert_models.py
+++ tests/test_bert_models.py
@@ -1,9 +1,10 @@
 import unittest
 
-from danlp.models import load_bert_emotion_model, load_bert_tone_model, load_bert_base_model, load_bert_ner_model, load_bert_nextsent_model, load_bert_offensive_model
+from danlp.models import load_bert_emotion_model, load_bert_tone_model, \
+    load_bert_base_model, load_bert_ner_model, load_bert_nextsent_model, \
+    load_bert_offensive_model, load_bert_hatespeech_model
 from danlp.download import DEFAULT_CACHE_DIR, download_model, \
     _unzip_process_func
-from transformers import BertTokenizer, BertForSequenceClassification
 import os
 
 class TestBertEmotion(unittest.TestCase):
@@ -125,6 +126,33 @@ class TestBertOffensive(unittest.TestCase):
         self.assertEqual(model.predict(sentence), "OFF")
         self.assertEqual(model._classes(), ['NOT', 'OFF'])
         self.assertTrue(len(model.predict_proba(sentence))==2)
-        
+
+class TestBertHatespeech(unittest.TestCase):
+
+    def test_download(self):
+        # Download model beforehand
+        for model in ['bert.hatespeech.detection', 'bert.hatespeech.classification']:
+            
+            model_path = download_model(model, DEFAULT_CACHE_DIR,
+                                        process_func=_unzip_process_func,
+                                        verbose=True)
+            
+            # check if path to model excist
+            self.assertTrue(os.path.exists(model_path))
+
+
+    def test_predictions(self):
+        model = load_bert_hatespeech_model()
+        sentence = "Han ejer ikke respekt for nogen eller noget... han er megaloman og psykopat"
+        pred = model.predict(sentence)
+        pred_proba = model.predict_proba(sentence)
+        self.assertEqual(pred['offensive'], "OFF")
+        self.assertEqual(pred['hatespeech'], "Sprogbrug")
+        self.assertEqual(model._classes()[0], ['NOT', 'OFF'])
+        self.assertEqual(model._classes()[1], ['Særlig opmærksomhed', 'Personangreb', 'Sprogbrug', 'Spam & indhold'])
+        self.assertTrue(len(pred_proba[0])==2)
+        self.assertTrue(len(pred_proba[1])==4)
+
+
 if __name__ == '__main__':
     unittest.main()
